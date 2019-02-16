0ed6dd45df622ac2f98e073ae2392d2a192ec31f
SuxrobGM <suxrobGM@gmail.com>
2019-02-16 18:01:45 +0500

    added paginationList

---
+++ SuxrobGM_SDK/Pagination/PaginatedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuxrobGM_SDK.Pagination
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }      
+
+        public PaginatedList(IList<T> items, int count, int pageIndex, int pageSize = 10)
+        {
+            PageIndex = pageIndex;           
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 1);
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }       
+
+        public static PaginatedList<T> Create(IQueryable<T> source, int pageIndex, int pageSize = 10)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageIndex - 1) * pageSize)
+                                    .Take(pageSize).ToList();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
