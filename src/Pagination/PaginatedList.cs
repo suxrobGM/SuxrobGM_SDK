3d24e04b8a78f0d29c0592fe561597022b1c9c5f
SuxrobGM <suxrobGM@gmail.com>
2019-04-14 20:54:44 +0500

    added AspNetCore Tag helpers, added Async version of PaginatedList

--- src/Pagination/PaginatedList.cs
+++ src/Pagination/PaginatedList.cs
@@ -1,25 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SuxrobGM.Sdk.Pagination
 {
     public class PaginatedList<T> : List<T>
     {
         public int PageIndex { get; private set; }
-        public int TotalPages { get; private set; }      
+        public int TotalPages { get; private set; }
 
-        public PaginatedList(IList<T> items, int count, int pageIndex, int pageSize = 10)
+        public bool HasPreviousPage { get => PageIndex > 1; }
+        public bool HasNextPage { get => PageIndex < TotalPages; }
+
+        private PaginatedList(IList<T> items, int count, int pageIndex, int pageSize = 10)
         {
             PageIndex = pageIndex;           
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
             this.AddRange(items);
         }
+                   
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize = 10)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize)
+                                    .Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
 
-        public bool HasPreviousPage { get => PageIndex > 1; }  
-        public bool HasNextPage { get => PageIndex < TotalPages; }           
-
-        public static PaginatedList<T> Create(IQueryable<T> source, int pageIndex, int pageSize = 10)
+        public static PaginatedList<T> Create(IEnumerable<T> source, int pageIndex, int pageSize = 10)
         {
             var count = source.Count();
             var items = source.Skip((pageIndex - 1) * pageSize)
