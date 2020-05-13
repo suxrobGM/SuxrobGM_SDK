0123b0d3f5ec56b6bd3e13fd0eda2b2348d575b2
SuxrobGM <suxrobGM@gmail.com>
2020-05-14 02:30:43 +0800

    added async method in PaginatedList class

--- src/SuxrobGM.Sdk/Pagination/PaginatedList.cs
+++ src/SuxrobGM.Sdk/Pagination/PaginatedList.cs
@@ -1,31 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SuxrobGM.Sdk.Pagination
 {
     public class PaginatedList<T> : List<T>
     {
-        public int PageIndex { get; private set; }
-        public int TotalPages { get; private set; }
+        public int PageIndex { get; }
+        public int TotalPages { get; }
 
-        public bool HasPreviousPage { get => PageIndex > 1; }
-        public bool HasNextPage { get => PageIndex < TotalPages; }
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
 
-        private PaginatedList(IList<T> items, int count, int pageIndex, int pageSize = 10)
+        private PaginatedList(IEnumerable<T> items, int count, int pageIndex, int pageSize = 10)
         {
             PageIndex = pageIndex;           
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
-            this.AddRange(items);
+            AddRange(items);
         }
-                   
-        //public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize = 10)
-        //{
-        //    var count = await source.CountAsync();
-        //    var items = await source.Skip((pageIndex - 1) * pageSize)
-        //                            .Take(pageSize).ToListAsync();
-        //    return new PaginatedList<T>(items, count, pageIndex, pageSize);
-        //}
+
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize = 10)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize)
+                                    .Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
 
         public static PaginatedList<T> Create(IEnumerable<T> source, int pageIndex, int pageSize = 10)
         {
